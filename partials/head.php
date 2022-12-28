a1f7e464728084f532c6b4c8fc2dc388291336be
ssanchezi <ssanchezi.gopartners@bancochile.cl>
2022-12-28 09:58:43 -0300

    Se agregan codigos de analytics y recaptcha

--- partials/head.php
+++ partials/head.php
@@ -2,8 +2,8 @@
 <title>Odontodent - <?php echo $title ?></title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 <meta name="author" content="Bruix Soluciones">
-<meta name="keywords" content="Implantes, endodoncias, protesis fijas, extracciones, restauraciones dentales, urgencia dental">
-<meta name="description" content="Odontodent es una clínica dental Ubicada en San Bernardo que presta los servicios de: Urgencia Dental, Ortodoncia, Estética, Coronas, Prótesis, Endodoncia, Prótesis Invisible, OdontoPediatría, Incrustaciones Estéticas, Cirugía Oral, Rayos X, Limpieza Dental, Tapaduras, Frenillos Invisibles, Blanqueamiento, Implantes Dentales, Brackets, Ortodoncia Lingual.">
+<meta name="keywords" content="endodoncias, protesis fijas, extracciones, restauraciones dentales, urgencia dental, dentista san bernardo, urgencia dental san bernardo, centro dentales san bernardo, urgencias dentales, centro dental, limpieza dental san bernardo, atención dental urgente, clinica urgencia dental, urgencia dental el bosque, clinica dental calera del tango, urgencia dental el bosque, urgencia dental calera de tango, atencion dental calera de tango, atencion dental el bosque, dentista calera de tango, dentista el bosque, urgencia dental buin, clinica dental buin, dentista buin, atencion dental buin, dentista buin, limpieza dental buin, limpieza dental calera de tango, limpieza dental el bosque, Estética dental, Coronas dentales, Prótesis dentales, Endodoncia, Prótesis Dental Invisible, Incrustaciones Estéticas Dentales, Cirugía Oral, Rayos X Dentales, Limpieza Dental, Tapaduras, Frenillos Invisibles, Blanqueamiento, Implantes Dentales, Brackets.">
+<meta name="description" content="Odontodent es una clínica dental Ubicada en San Bernardo que presta los servicios de: Urgencia Dental, Ortodoncia, Estética dental, Coronas dentales, Prótesis dentales, Endodoncia, Prótesis Dental Invisible, Incrustaciones Estéticas Dentales, Cirugía Oral, Rayos X Dentales, Limpieza Dental, Tapaduras, Frenillos Invisibles, Blanqueamiento, Implantes Dentales, Brackets.">
 <meta name="copyright" content="Bruix Soluciones" />
 
 <!-- Favicon -->
@@ -28,3 +28,26 @@
 
 <!-- Template Stylesheet -->
 <link href="css/style.css" rel="stylesheet">
+
+<!-- Google recaptcha v3 -->
+<script src="https://www.google.com/recaptcha/api.js?render=6Le3y7MjAAAAANuyR993rA-AHAZ3epBNON-Jw1ZM"></script>
+<script>
+      function onClick(e) {
+        e.preventDefault();
+        grecaptcha.ready(function() {
+          grecaptcha.execute('6Le3y7MjAAAAANuyR993rA-AHAZ3epBNON-Jw1ZM', {action: 'submit'}).then(function(token) {
+              // Add your logic to submit to your backend server here.
+          });
+        });
+      }
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-T0FBFGLP03"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-T0FBFGLP03');
+</script>
